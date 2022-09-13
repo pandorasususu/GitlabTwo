7f8c8f7c3cad6237e75439f3907af48545b63dfc
LBH <hitlbh777@gmail.com>
2022-09-13 18:21:24 +0900

    etc: router element 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginButton from './pages/LoginPage.jsx'
+import LoginButton from "pages/LoginPage.jsx";
+import HistoryPage from "pages/HistoryPage.jsx";
+import NearAnalysisPage from "pages/NearAnalysisPage.jsx";
+import OtherUserPage from "pages/OtherUserPage.jsx";
 
 function App() {
   return (
     <div className="App">
-      <LoginButton/>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" />
+          <Route path="/login" element={<LoginButton />} />
           <Route path="/guide" />
           <Route path="/info" />
           <Route path="/main" />
           <Route path="/recommend" />
-          <Route path="/history" />
-          <Route path="/near" />
-          <Route path="/other" />
+          <Route path="/history" element={<HistoryPage/>} />
+          <Route path="/near" element={<NearAnalysisPage/>} />
+          <Route path="/other" element={<OtherUserPage/>} />
         </Routes>
       </BrowserRouter>
-  </div>
+    </div>
   );
 }
 
