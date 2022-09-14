af417f6e731f113a8fccf46fa8033a03c48760ad
InhuJo <inhudev@gmail.com>
2022-09-14 15:46:08 +0900

    [S07P22D104-218] design: 컴포넌트 스타일 적용
    - 슬라이더, 버튼 색깔 변경

--- frontend/src/components/MainPage/SearchLocation.js
+++ frontend/src/components/MainPage/SearchLocation.js
@@ -3,7 +3,7 @@ import { TextField } from "@mui/material";
 function SearchLocation() {
   return (
     <div className="search__location">
-      <div className="search__title">검색 기준 위치</div>
+      <div className="search__title title--top">검색 기준</div>
       <TextField size="small" placeholder="구미시" sx={{width: "100%"}}/>
     </div>
   );
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,20 +1,40 @@
-import { Button, Grid } from "@mui/material";
-import BottomNav from "components/common/BottomNav";
-import Container from "components/common/Container";
-import Map from "components/MainPage/Map";
-import SearchLocation from "components/MainPage/SearchLocation";
-import SearchRange from "components/MainPage/SearchRange";
-import "styles/MainPage/MainPage.scss";
+import { Button, Grid } from '@mui/material';
+import styled from '@emotion/styled';
+import BottomNav from 'components/common/BottomNav';
+import Container from 'components/common/Container';
+import Map from 'components/MainPage/Map';
+import SearchLocation from 'components/MainPage/SearchLocation';
+import SearchRange from 'components/MainPage/SearchRange';
+import 'styles/MainPage/MainPage.scss';
+
+const RecommendButton = styled(Button)`
+  background-color: #92b4ec;
+  border: none;
+  box-shadow: 0px 2px 2px 0px rgb(0 0 0 / 14%);
+  font-weight: 600;
+
+  &.MuiButton-root:hover {
+    background-color: #92b4ec;
+    box-shadow: 0px 2px 2px 0px rgb(0 0 0 / 14%);
+  }
+`;
 
 function MainPage() {
   return (
     <Container>
       <Map />
       <div className="search">
-        <Grid container className="search-inner" direction="column" justifyContent="center">
+        <Grid
+          container
+          className="search-inner"
+          direction="column"
+          justifyContent="center"
+        >
           <SearchLocation />
           <SearchRange />
-          <Button variant="contained" size="large">추천 시작</Button>
+          <RecommendButton variant="contained" size="large">
+            추천 시작
+          </RecommendButton>
         </Grid>
       </div>
       <BottomNav />
