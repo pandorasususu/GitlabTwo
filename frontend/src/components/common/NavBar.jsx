02352dacdce8d0920378ae7e2789c250fb2c17fb
lbh <hitlbh@naver.com>
2022-09-14 10:35:51 +0900

    [S07P22D104-258] feat: 하단메뉴바 레이아웃 mui에서 가져와 icon 및 소개 문구만 구현

--- frontend/src/components/common/NavBar.jsx
+++ frontend/src/components/common/NavBar.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import AssistantOutlinedIcon from '@mui/icons-material/AssistantOutlined';
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 
 const NavBar = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <>
-      <h1>메뉴</h1>
+    <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
+      <Tab icon={<AssistantOutlinedIcon />} label="추천받기" />
+      <Tab icon={<HistoryOutlinedIcon />} label="다시보기" />
+      <Tab icon={<LocationOnOutlinedIcon />} label="동네분석" />
+      <Tab icon={<PersonPinIcon />} label="다른일정" />
+    </Tabs>
     </>
   );
 };
