a7fd4025b50fb240be8a3c17f0e7624ab4f7ce7e
InhuJo <inhudev@gmail.com>
2022-09-14 10:42:57 +0900

    [S07P22D104-218] feat: 메인페이지 컴포넌트 생성

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import Container from "components/common/Container";
+import "styles/MainPage/MainPage.scss";
+
+function MainPage() {
+  return (
+    <Container>main</Container>
+  );
+}
+
+export default MainPage;
