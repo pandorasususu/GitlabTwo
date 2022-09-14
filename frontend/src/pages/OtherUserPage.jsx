d66b10547917eaf90fd7f292c9aba5799aa5440d
lbh <hitlbh@naver.com>
2022-09-14 16:41:05 +0900

    [S07P22D104-57] feat: 타유저선택 레이아웃

--- frontend/src/pages/OtherUserPage.jsx
+++ frontend/src/pages/OtherUserPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NavBar from "components/common/NavBar.jsx";
 
 function OtherUserPage() {
-  return <h1>타유저 선택</h1>;
+  return (
+    <>
+    <h1>타유저 선택</h1>
+    <NavBar/>
+    </>
+  );
 }
 
 export default OtherUserPage;
