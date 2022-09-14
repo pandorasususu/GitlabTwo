6e3894e1ea87343ac4046a305545894a6e4cf9a2
InhuJo <inhudev@gmail.com>
2022-09-14 17:52:13 +0900

    [S07P22D104-220] feat: 사용자 현재 위치 받아오는 useCurrentLocation 훅 구현
    - 참조 url: https://jw910911.tistory.com/108

--- frontend/src/components/MainPage/Map.js
+++ frontend/src/components/MainPage/Map.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import useCurrentLocation from 'hook/useCurrentLocation';
+import React, { useEffect } from 'react';
 
 const { kakao } = window;
 
+const geolocationOptions = {
+  enableHighAccuracy: true,
+  timeout: 1000 * 60 * 1, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
+  maximumAge: 1000 * 3600 * 24, // 24 hour
+}
+
 const Map = () => {
+  const { location, error } = useCurrentLocation(geolocationOptions);
+  console.log(location);
+
   useEffect(() => {
-    let container = document.getElementById("map");
+    let container = document.getElementById('map');
     let options = {
       center: new window.kakao.maps.LatLng(
         37.365264512305174,
