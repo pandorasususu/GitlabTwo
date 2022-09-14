718f40829d26d5a7883a02ec2bb675fe978c9655
InhuJo <inhudev@gmail.com>
2022-09-14 13:30:01 +0900

    [S07P22D104-218] feat: 검색 주소, 반경 컴포넌트 추가

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,12 +1,36 @@
 import BottomNav from "components/common/BottomNav";
 import Container from "components/common/Container";
 import Map from "components/MainPage/Map";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Slider from "@mui/material/Slider";
 import "styles/MainPage/MainPage.scss";
+
+function valuetext(value) {
+  return `${value}`;
+}
 
 function MainPage() {
   return (
     <Container>
-      <Map/>
+      <Map />
+      <div className="search">
+        <Grid direction="column">
+          <div>검색 범위 지역</div>
+          <TextField hiddenlabel size="small" placeholder="구미시" />
+          <div>검색 반경</div>
+          <Slider
+            aria-label="Temperature"
+            defaultValue={2}
+            getAriaValueText={valuetext}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+            min={1}
+            max={5}
+          />
+        </Grid>
+      </div>
       <BottomNav />
     </Container>
   );
