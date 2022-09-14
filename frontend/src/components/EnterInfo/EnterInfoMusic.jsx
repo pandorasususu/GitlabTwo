e700649832b7771f35894f292e8f60f65dba6470
임현탁 <seoktak123@gmail.com>
2022-09-14 16:43:11 +0900

    [S07P22D104-150] feat: 음악 선호도 조사 폼 구성

--- frontend/src/components/EnterInfo/EnterInfoMusic.jsx
+++ frontend/src/components/EnterInfo/EnterInfoMusic.jsx
@@ -1,7 +1,17 @@
+import EnterInfoMusicCard from './EnterInfoMusicCard.jsx'
+
 const EnterInfo = () => {
     return (
         <div>
-            해윙
+            <div>
+                <h1>어떤 음악을</h1>
+                <h1>자주 들으시나요?</h1>
+            </div>
+            <div>
+                <div>
+                    <EnterInfoMusicCard/>
+                </div>
+            </div>
         </div>
     );
 };
