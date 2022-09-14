9d1211fafdda8620ee8edc7bcf0964305932d42c
lbh <hitlbh@naver.com>
2022-09-14 14:06:33 +0900

    [S07P22D104-47] feat: PWA 적용 service-worker.js serviceWorkerRegistration.js를 src 폴더 아래에 추가 index.js에 serviceWorkerRegistration.register(); 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as serviceWorkerRegistration from 'serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -8,3 +9,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// PWA가 오프라인에서 작동하기 위해 필요한 구문
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
