94308a056c7d39f94ca74a2af613dc78995084a0
lbh <hitlbh@naver.com>
2022-09-14 11:30:07 +0900

    [S07P22D104-260] feat: 하단메뉴바 기능 구현 메뉴 탭에 해당하는 페이지일 때 css 변하는 것은 아직 미구현

--- frontend/src/components/common/NavBar.jsx
+++ frontend/src/components/common/NavBar.jsx
@@ -1,25 +1,35 @@
-import * as React from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import AssistantOutlinedIcon from '@mui/icons-material/AssistantOutlined';
 import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import PersonPinIcon from '@mui/icons-material/PersonPin';
+import { useNavigate, Link   } from "react-router-dom";
 
 const NavBar = () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const [value, setValue] = useState(0);
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
+  function LinkTab(props) {
+    return (
+      <Tab
+      component={Link}
+      to={props.href}
+      icon={props.icon}
+      label={props.label}
+    />
+    )
+  }
   return (
     <>
     <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
-      <Tab icon={<AssistantOutlinedIcon />} label="추천받기" />
-      <Tab icon={<HistoryOutlinedIcon />} label="다시보기" />
-      <Tab icon={<LocationOnOutlinedIcon />} label="동네분석" />
-      <Tab icon={<PersonPinIcon />} label="다른일정" />
+      <Tab icon={<AssistantOutlinedIcon />} label="추천받기" href="/main"/>
+      <LinkTab icon={<HistoryOutlinedIcon />} label="다시보기" href="/history"/>
+      <LinkTab icon={<LocationOnOutlinedIcon />} label="동네분석" href="/near"/>
+      <LinkTab icon={<PersonPinIcon />} label="다른일정" href="/other"/>
     </Tabs>
     </>
   );
