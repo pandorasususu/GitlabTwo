eeac86c0eb4acbf81b4c759f176359d8fa48e272
임현탁 <seoktak123@gmail.com>
2022-09-15 17:05:52 +0900

    [S07P22D104-259] feat: drag and drop 실험

--- frontend/src/pages/EnterInfoGuidePage.jsx
+++ frontend/src/pages/EnterInfoGuidePage.jsx
@@ -6,7 +6,7 @@ import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'styles/EnterInfoGuidePage/EnterInfoGuidePage.scss';
-import Container from 'components/EnterInfoGuide/Container';
+import Container from 'components/common/Container';
 
 const EnterInfoGuidePage = () => {   
     function NextArrow(props) {
