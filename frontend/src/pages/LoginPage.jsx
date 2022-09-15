12fd014a1f3c6ca884e6f7c442b3ddc2ad1e99eb
임현탁 <seoktak123@gmail.com>
2022-09-15 09:28:47 +0900

    [S07P22D104] feat: 로그인페이지 css적용 준비

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,6 +3,8 @@ import GoogleLogin from 'react-google-login';
 import { gapi } from 'gapi-script';
 import logo from '../assets/images/Logo.png'
 import mascot from '../assets/images/WalkingGirl.gif'
+import Container from 'components/common/Container';
+import 'styles/MainPage/MainPage.scss';
 
 const clientId = '835634401246-ddaeprck32cbkjmajefeffl5vh7f5kd6.apps.googleusercontent.com'
 
@@ -26,14 +28,16 @@ const LoginPage = ({ onSocial }) => {
     };
     return (
         <div>
-            <img src={logo} alt="Logo" />
-            <img src={mascot} alt="Mascot" />
-            <GoogleLogin
-                clientId={clientId}
-                buttonText="로그인하기"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-            />
+            <Container>
+                <img classname="" src={logo} alt="Logo" />
+                <img classname="" src={mascot} alt="Mascot" />
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="로그인하기"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                />
+            </Container>
         </div>
     );
 };
