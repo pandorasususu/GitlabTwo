e6a812fb664deb282d772de356ac4f3fe7ae9395
임현탁 <seoktak123@gmail.com>
2022-09-16 14:17:10 +0900

    [S07P22D104-148] feat: 음악 선호도 조사 css 기본적용

--- frontend/src/components/EnterInfo/EnterInfoFood.jsx
+++ frontend/src/components/EnterInfo/EnterInfoFood.jsx
@@ -8,12 +8,10 @@ const EnterInfo = () => {
                 <h2>어떤 음식을</h2>
                 <h2>자주 드시나요?</h2>
             </div>
-            <div>
                 <div className='UserInput__Food__Item'>
                     {/* <EnterInfoFoodCard/> */}
                     <img className="UserInput__Food__Item__Mascot" src={mascot} alt="Mascot" />
                 </div>
-            </div>
         </div>
     );
 };
--- frontend/src/components/EnterInfo/EnterInfoMusic.jsx
+++ frontend/src/components/EnterInfo/EnterInfoMusic.jsx
@@ -1,17 +1,17 @@
 import EnterInfoMusicCard from './EnterInfoMusicCard.jsx'
+import mascot from '../../assets/images/WalkingGirl.gif'
 
 const EnterInfo = () => {
     return (
         <div>
-            <div>
-                <h1>어떤 음악을</h1>
-                <h1>자주 들으시나요?</h1>
+            <div className='UserInput__Music__Title'>
+                <h2>어떤 음악을</h2>
+                <h2>자주 들으시나요?</h2>
             </div>
-            <div>
-                <div>
-                    <EnterInfoMusicCard/>
+                <div className='UserInput__Music__Item'>
+                    {/* <EnterInfoMusicCard/> */}
+                    <img className="UserInput__Music__Item__Mascot" src={mascot} alt="Mascot" />
                 </div>
-            </div>
         </div>
     );
 };
