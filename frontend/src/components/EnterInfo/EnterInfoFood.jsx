f58e03016ceccd83605593c93716b421229c981e
임현탁 <seoktak123@gmail.com>
2022-09-16 14:15:00 +0900

    [S07P22D104-152] feat: 음식 선호도 조사 css 기본적용

--- frontend/src/components/EnterInfo/EnterInfoFood.jsx
+++ frontend/src/components/EnterInfo/EnterInfoFood.jsx
@@ -1,15 +1,17 @@
 import EnterInfoFoodCard from './EnterInfoFoodCard.jsx'
+import mascot from '../../assets/images/WalkingGirl.gif'
 
 const EnterInfo = () => {
     return (
         <div>
-            <div>
-                <h1>어떤 음식을</h1>
-                <h1>자주 드시나요?</h1>
+            <div className='UserInput__Food__Title'>
+                <h2>어떤 음식을</h2>
+                <h2>자주 드시나요?</h2>
             </div>
             <div>
-                <div>
-                    <EnterInfoFoodCard/>
+                <div className='UserInput__Food__Item'>
+                    {/* <EnterInfoFoodCard/> */}
+                    <img className="UserInput__Food__Item__Mascot" src={mascot} alt="Mascot" />
                 </div>
             </div>
         </div>
