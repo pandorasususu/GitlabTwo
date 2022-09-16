56e2106eb58284fba3bf5b1b8fd16cc4d4a66c8e
임현탁 <seoktak123@gmail.com>
2022-09-16 11:09:57 +0900

    [S07P22D104-261] feat: 사용자인풋가이드 두번째페이지 css 적용

--- frontend/src/components/EnterInfoGuide/EnterInfoGuideSecond.jsx
+++ frontend/src/components/EnterInfoGuide/EnterInfoGuideSecond.jsx
@@ -1,13 +1,14 @@
-
+import mascot from '../../assets/images/WalkingGirl.gif'
 
 const EnterInfoGuideSecond = () => {
     return (
         <div>
-            <div>
-                <div>
-                    <h1>세가지 카테고리에 대한</h1>
-                    <h1>선택지를 드릴게요</h1>
-                </div>
+            <div className='Guide__Title'>
+                <h2>세가지 카테고리에 대한</h2>
+                <h2>선택지를 드릴게요</h2>
+            </div>
+            <div className="Guide__Second__Item">
+                <img className="Guide__Second__Item__Mascot" src={mascot} alt="Mascot" />
             </div>
         </div>
         
