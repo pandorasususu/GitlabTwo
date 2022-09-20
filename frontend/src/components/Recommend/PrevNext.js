3d8542b33dba5e4e02aef6e7a9754ead94334d03
조인후 <inhudev@gmail.com>
2022-09-20 15:14:15 +0900

    [S07P22D104-228] feat: 하단 페이지 표시 추가

--- frontend/src/components/Recommend/PrevNext.js
+++ frontend/src/components/Recommend/PrevNext.js
@@ -26,6 +26,7 @@ function PrevNext() {
       <PlainButton startIcon={<ArrowBackIosNewIcon />} onClick={handlePrev}>
         이전
       </PlainButton>
+      <div className="recommend-bottom__page">1 / 3</div>
       <PlainButton endIcon={<ArrowForwardIosIcon />} onClick={handleNext}>
         다음
       </PlainButton>
