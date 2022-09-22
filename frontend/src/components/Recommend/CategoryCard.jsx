9a4768eddb9e143e0b1bb6a3d628980ff2bc79c7
조인후 <inhudev@gmail.com>
2022-09-22 13:44:32 +0900

    [S07P22D104-231] feat: 카테고리 컴포넌트에 좋아요, 싫어요 버튼 추가

--- frontend/src/components/Recommend/CategoryCard.jsx
+++ frontend/src/components/Recommend/CategoryCard.jsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
+
 export default function CategoryCard() {
+  const [like, setLike] = useState(false);
+  const [dislike, setDislike] = useState(false);
+
+  const handleLike = () => {
+    if (dislike) setDislike(false);
+    setLike(!like);
+  };
+
+  const handleDislike = () => {
+    if (like) setLike(false);
+    setDislike(!dislike);
+  };
+
   return (
     <div className="category-card">
       <div className="category-card__img-wrapper">사진</div>
       <div className="category-card__category-desc">
-        <div>카테고리 이름</div>
-        <div>굿 밷</div>
+        <div className="category-desc__inner">
+          <span>카테고리 이름</span>
+          <div className="category-desc__like">
+            <IconButton onClick={handleLike}>
+              {!like && <ThumbUpOffAltIcon />}
+              {like && <ThumbUpAltIcon />}
+            </IconButton>
+            <IconButton onClick={handleDislike}>
+              {!dislike && <ThumbDownOffAltIcon />}
+              {dislike && <ThumbDownAltIcon />}
+            </IconButton>
+          </div>
+        </div>
       </div>
     </div>
   );
