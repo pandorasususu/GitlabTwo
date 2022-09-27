089f1ce945ec61199416117f4c3ad7f1c2e50ce6
조인후 <inhudev@gmail.com>
2022-09-27 16:56:00 +0900

    [S07P22D104-234] feat: 가게 리스트 눌렀을 때 지도로 이동

--- frontend/src/components/Recommend/Category/Detail/index.jsx
+++ frontend/src/components/Recommend/Category/Detail/index.jsx
@@ -15,6 +15,49 @@ const CustomDrawer = styled(Drawer)`
   }
 `;
 
+const list = [
+  {
+    foodId: '1',
+    foodName: '이태리 제면소',
+    foodAddress: '대구 달성군 화원읍 비슬로 2528',
+    foodLatitude: '35.8012585',
+    foodLongtitude: '128.494283',
+    foodTime: '매일 11:10 ~ 21:00',
+  },
+  {
+    foodId: '2',
+    foodName: '황장군',
+    foodAddress: '대구 달성군 화원읍 비슬로 2530',
+    foodLatitude: '35.8014342',
+    foodLongtitude: '128.494651',
+    foodTime: '매일 08:00 ~ 21:30',
+  },
+  {
+    foodId: '3',
+    foodName: '코스모스막창',
+    foodAddress: '대구 달성군 화원읍 명천로 226',
+    foodLatitude: '35.7993167',
+    foodLongtitude: '128.493904',
+    foodTime: '매일 15:30 ~ 00:30',
+  },
+  {
+    foodId: '4',
+    foodName: '투썸플레이스',
+    foodAddress: '대구 달성군 화원읍 비슬로 2529',
+    foodLatitude: '35.8019464',
+    foodLongtitude: '128.494041',
+    foodTime: '매일 08:30 ~ 00:30',
+  },
+  {
+    foodId: '5',
+    foodName: '이디야 커피',
+    foodAddress: '대구 달성군 화원읍 비슬로 2503',
+    foodLatitude: '35.8004907',
+    foodLongtitude: '128.491525',
+    foodTime: '매일 09:00 ~ 22:30',
+  },
+];
+
 export default function CategoryDetail({ current, open, handleClose }) {
   const [isList, setIsList] = useState(true);
 
@@ -37,8 +80,8 @@ export default function CategoryDetail({ current, open, handleClose }) {
         handleClick={handleClick}
         handleClose={ToggleDrawer}
       />
-      {isList && <CategoryStoreList list={current.store} />}
-      {!isList && <StoreMap list={current.store} />}
+      {isList && <CategoryStoreList list={list} handleClick={handleClick} />}
+      {!isList && <StoreMap list={list} />}
     </CustomDrawer>
   );
 }
