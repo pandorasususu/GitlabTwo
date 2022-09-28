22170f59f97399b1ca341c812c99d194ecd53920
lbh <hitlbh@naver.com>
2022-09-28 15:57:10 +0900

    etc: intellj 스웨거 껍데기만 돌릴 수 있게 수정

--- backend/src/main/java/com/ssafy/db/entity/User.java
+++ backend/src/main/java/com/ssafy/db/entity/User.java
@@ -1,22 +1,22 @@
-package com.ssafy.db.entity;
-
-import javax.persistence.*;
-
-@Entity
-@Table (name = "User")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    int userId;
-
-    @Column(name = "user_email")
-    String userEmail;
-
-    @Column(name = "user_nickname")
-    String userNickname;
-
-    @Column(name = "user_img")
-    String userImg;
-}
+//package com.ssafy.db.entity;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table (name = "User")
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "user_id")
+//    int userId;
+//
+//    @Column(name = "user_email")
+//    String userEmail;
+//
+//    @Column(name = "user_nickname")
+//    String userNickname;
+//
+//    @Column(name = "user_img")
+//    String userImg;
+//}
