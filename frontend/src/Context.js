4f208ba5f63d18559b7d44a88383e6df4059cff2
조인후 <inhudev@gmail.com>
2022-09-29 11:55:04 +0900

    [S07P22D104-132] feat: context에 스포티파이, 로그인 토큰 저장

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -1,14 +1,27 @@
 import { createContext, useContext, useMemo, useState } from 'react';
 
-const Context = createContext();
+const SpotifyContext = createContext();
+const LoginContext = createContext();
 
 export function ContextProvider({ children }) {
-  const [token, setToken] = useState();
-  const store = useMemo(() => ({ token, setToken }), [token]);
+  const [spotify, setSpotify] = useState();
+  const [login, setLogin] = useState();
+  const spotifyStore = useMemo(() => ({ spotify, setSpotify }), [spotify]);
+  const loginStore = useMemo(() => ({ login, setLogin }), [login]);
 
-  return <Context.Provider value={store}>{children}</Context.Provider>;
+  return (
+    <SpotifyContext.Provider value={spotifyStore}>
+      <LoginContext.Provider value={loginStore}>
+        {children}
+      </LoginContext.Provider>
+    </SpotifyContext.Provider>
+  );
 }
 
-export function useSpotifyContext() {
-  return useContext(Context);
+export function useSpotifyToken() {
+  return useContext(SpotifyContext);
+}
+
+export function useLoginToken() {
+  return useContext(LoginContext);
 }
--- frontend/src/components/Login/SpotifyRedirectHandler.jsx
+++ frontend/src/components/Login/SpotifyRedirectHandler.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSpotifyContext } from 'Context';
+import { useLoginToken, useSpotifyToken } from 'Context';
 import { getSpotifyToken } from 'api/spotify';
 import { getApiInstance, getSpotifyApi } from 'api';
 import queryString from 'query-string';
 
 export default function SpotifyRedirectHandler() {
-  const { setToken } = useSpotifyContext();
-  const navigate = useNavigate();
+  const { setSpotify } = useSpotifyToken();
+  const { setLogin } = useLoginToken();
   const param = new URLSearchParams(window.location.search);
   const option = queryString.stringify({
     code: param.get('code'),
@@ -22,7 +21,7 @@ export default function SpotifyRedirectHandler() {
         // 스포티파이 토큰 저장
         const access_token = res.data.access_token;
         const refresh_token = res.data.refresh_token;
-        setToken(access_token);
+        setSpotify(access_token);
         localStorage.setItem('refresh_token', refresh_token);
         // 스포티파이에서 유저 정보 얻기
         const spotify = getSpotifyApi(access_token);
@@ -33,8 +32,9 @@ export default function SpotifyRedirectHandler() {
           const login = await getApiInstance().post('/user', email);
           // 로그인 성공 시, 첫 로그인 여부에 따라서 페이지 다르게 이동
           if (login.status === 200) {
-            localStorage.setItem('token', login.data.accessToken);
-            navigate('/guide/first');
+            setLogin(login.data.accessToken);
+            window.location.replace('/guide/first');
+            // 로그인 실패 시
           } else {
             alert('로그인에 실패했습니다.');
             window.location.replace('/');
