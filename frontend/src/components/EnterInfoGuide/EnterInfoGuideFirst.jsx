3a77f79f1077a584256e7b9c2d4e3bbf76265235
임현탁 <seoktak123@gmail.com>
2022-10-06 16:14:14 +0900

    etc: 서비스가이드라인 문구 수정

--- frontend/src/components/EnterInfoGuide/EnterInfoGuideFirst.jsx
+++ frontend/src/components/EnterInfoGuide/EnterInfoGuideFirst.jsx
@@ -31,7 +31,7 @@ const EnterInfoGuideFirst = () => {
           </div>
           <div className="Guide__First__Alarm">
             <ErrorOutlineIcon color="error" fontSize="small" />
-            <p>서비스 아용 가이드라인</p>
+            <p>서비스 가이드라인</p>
           </div>
           <div className="Guide__First__Item">
             <img
