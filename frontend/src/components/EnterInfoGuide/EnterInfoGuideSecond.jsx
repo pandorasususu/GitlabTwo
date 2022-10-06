cc1d7a8f57b584cdc2e1aef8c1c470b431123e1e
조인후 <inhudev@gmail.com>
2022-10-06 13:50:06 +0900

    design: 취향조사 페이지 CSS 수정

--- frontend/src/components/EnterInfoGuide/EnterInfoGuideSecond.jsx
+++ frontend/src/components/EnterInfoGuide/EnterInfoGuideSecond.jsx
@@ -1,4 +1,4 @@
-import mascot from '../../assets/images/guide_second.jpg'
+import mascot from '../../assets/images/guide_second.jpg';
 import { Button } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -18,28 +18,38 @@ const PlainButton = styled(Button)`
 const EnterInfoGuideSecond = () => {
   const handlePrev = () => {
     console.log('이전');
-    window.location.href ="/guide/first"
+    window.location.href = '/guide/first';
   };
-    
+
   const handleNext = () => {
     console.log('다음');
-    window.location.href ="/guide/third"
+    window.location.href = '/guide/third';
   };
   return (
     <div>
       <Container>
         <div className="Guide">
-          <div className='Guide__Second__Title'>
+          <div className="Guide__Second__Title">
             <h2>세가지 카테고리에</h2>
             <h2>대한 선택지를</h2>
-            <h2 className='Guide__Second__Title__Bottom'>드릴거에요!!!!</h2>
+            <h2 className="Guide__Second__Title__Bottom">드릴게요!</h2>
           </div>
-          <div className='Guide__First__Alarm'><ErrorOutlineIcon color="error" fontSize="small"/><p>튜토리얼 입니다!!</p></div>
+          <div className="Guide__First__Alarm">
+            <ErrorOutlineIcon color="error" fontSize="small" />
+            <p>서비스 아용 가이드라인</p>
+          </div>
           <div className="Guide__Second__Item">
-            <img className="Guide__Second__Item__Mascot" src={mascot} alt="Mascot" />
+            <img
+              className="Guide__Second__Item__Mascot"
+              src={mascot}
+              alt="Mascot"
+            />
           </div>
           <div className="recommend-bottom">
-            <PlainButton startIcon={<ArrowBackIosNewIcon />} onClick={handlePrev}>
+            <PlainButton
+              startIcon={<ArrowBackIosNewIcon />}
+              onClick={handlePrev}
+            >
               이전
             </PlainButton>
             <PlainButton endIcon={<ArrowForwardIosIcon />} onClick={handleNext}>
@@ -48,7 +58,7 @@ const EnterInfoGuideSecond = () => {
           </div>
         </div>
       </Container>
-    </div>   
+    </div>
   );
 };
 
